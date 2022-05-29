a6e65511549b4b6d714a640f6bdf14f2acf92513
EndesaptTest <103508859+EndesaptTest@users.noreply.github.com>
2022-05-29 10:21:44 +0300

    Update plots.js

--- scripts/plots.js
+++ scripts/plots.js
@@ -2,8 +2,10 @@
 function recreateFunctionText(function_text)
 {
     function_text=function_text.replace(/,/g,';')
+    function_text=function_text.replace(/ /g,'')
     function_text=function_text.replace(/.*y=/gm,'');
-    function_text=function_text.replace(/([(?:\w\d\.|x)]+?)\^([?:\d\.|x)]+)/gm,'Math.pow($1,$2)');
+    //function_text=function_text.replace(/([(?:\w\d\.|x)]+?)\^(?:[\d\.|x)]+)/gm,'Math.pow($1,$2)');
+    function_text=function_text.replace(/((?:[^\+\-\*\/+]+)|(?:\([\d\w\.\+\-\*\/]+\)))\^((?:\([\d\w\.\+\-\*\/]+\))|(?:-*[^\+\-\*\/+]+))/gm,'Math.pow($1,$2)');
     function_text=function_text.replace(/sqrt\((.+?)\)/gm,'Math.sqrt($1)');
     function_text=function_text.replace(/sin\((.+?)\)/gm,'Math.sin($1)');
     function_text=function_text.replace(/cos\((.+?)\)/gm,'Math.cos($1)');
@@ -48,6 +50,7 @@ function makePlotFromFunction(function_text){
             ctx.fillText(`${i-8}`,i*20-8,146);
         }
         ctx.rotate(-Math.PI/2 );
+        ctx.stroke();
         //начало отрисовки графика
        
         yoffset=-150;
@@ -57,11 +60,11 @@ function makePlotFromFunction(function_text){
             let y=eval(function_text[1].replace(/x/g,x));
             let y1;
             ctx.moveTo(y*20+yoffset,x*20);
-            for(let x=-10;x<10;x+=0.06){
+            for(let x=-8;x<8;x+=0.06){
                 y1=y;
                 y=eval(function_text[1].replace(/x/g,x));
                 
-                if(isNaN(y1) ||Math.abs(y1-y)>40 ){
+                if(isNaN(y1) ||Math.abs(y1-y)>100 ){
                     
                     ctx.moveTo(y*20+yoffset,x*20+xoffset);
                 }
